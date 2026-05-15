3b23fe999751f0ff81468b599f677e612bdf89cd
jwat0 <jwat0@LAPTOP-TI91O9G8>
2026-05-15 10:19:53 -0400

    Add project files.

---
+++ EMSapi/Controllers/EMSController.cs
@@ -0,0 +1,95 @@
+﻿using EMSapi.Data;
+using EMSapi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMSapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EMSController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EMSController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null) return NotFound();
+            return employee;
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int Id, Employee employee)
+        {
+
+            if (Id != employee.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Employees.Any(e => e.Id == employee.Id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return NoContent();
+
+
+
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee (int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool EmployeeExists(object id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EMSapi/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace EMSapi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string JobTitle { get; set; } = string.Empty;
+
+        public string Department { get; set; } = string.Empty;
+
+        public decimal Salary { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public string EmploymentStatus { get; set; } = "Active";
+
+    }
+}
